2d5a8a3f5913ced8de8db45fc823ef1015b3afc1
Thomas Marks <marksta@umich.edu>
2024-06-05 17:16:24 -0400

    Updata JSON input for HallThruster.jl (#9)
    * Update thruster.py for compatability with new JSON input
    
    * Update juliapkg.json
    
    * fix || to or
    
    * Update thruster.py
    
    * syntax error
    
    * a comma
    
    * test-driven development
    
    * Update hallthruster_jl.json
    
    * Update variables_v0.json
    
    * Update hallthruster_jl.json
    
    * Update juliapkg.json
    
    * commas again
    
    * Update key for neutral ingestion multiplier
    
    * Update test_models.py
    
    * Update test_models.py
    
    * Update test_models.py
    
    * Update test_models.py
    
    * fix default/required inputs for hallthruster.jl config
    
    * small fix in json input
    
    ---------
    
    Co-authored-by: Joshua Eckels <eckelsjd@umich.edu>

--- tests/test_models.py
+++ tests/test_models.py
@@ -59,13 +59,14 @@ def test_cc(plots=False):
 
 
 def test_hallthruster_jl(plots=False):
+    """This test must coincide with the `required_inputs` of the config file passed to `hallthruster_jl_wrapper`"""
     variables = load_variables(["PB", "Va", "mdot_a", "T_ec", "u_n", "l_t", "vAN1", "vAN2", "delta_z",
                                 "z0", "p0", "V_cc"], CONFIG_DIR / 'variables_v0.json')
     x = np.empty((2, len(variables)))
     for i, var in enumerate(variables):
         x[:, i] = var.sample_domain(2)
     alpha = (0, 0)
-    res = hallthruster_jl_wrapper(x, alpha, n_jobs=2)
+    res = hallthruster_jl_wrapper(x, alpha, n_jobs=2, config=CONFIG_DIR / 'hallthruster_jl.json')
 
     if plots:
         fig, ax = plt.subplots()
