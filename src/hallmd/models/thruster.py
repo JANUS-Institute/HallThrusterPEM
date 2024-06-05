3771ef22e53dff2e373c4afca9d1fed5679a7068
Thomas Marks <marksta@umich.edu>
2024-06-05 15:02:24 -0400

    Update key for neutral ingestion multiplier

--- src/hallmd/models/thruster.py
+++ src/hallmd/models/thruster.py
@@ -67,7 +67,7 @@ def hallthruster_jl_input(thruster_input: dict) -> dict:
         'anom_model_coeffs': anom_model_coeffs,
         'background_pressure_Torr': 10 ** thruster_input['PB'],
         'background_temperature_K': thruster_input['background_temperature_K'],
-        'neutral_ingestion_multiplier': thruster_input['neutral_ingestion_multiplier'],
+        'neutral_ingestion_multiplier': thruster_input['f_n'],
         'apply_thrust_divergence_correction': thruster_input['apply_thrust_divergence_correction'],
         # design
         'thruster_name': thruster_input['thruster_name'],
