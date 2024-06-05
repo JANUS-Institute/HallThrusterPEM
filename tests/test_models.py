23c348ac9a1b9795e07d8e1213d1b38b4accdfcc
Thomas Marks <marksta@umich.edu>
2024-06-05 15:12:35 -0400

    Update test_models.py

--- tests/test_models.py
+++ tests/test_models.py
@@ -60,7 +60,7 @@ def test_cc(plots=False):
 
 def test_hallthruster_jl(plots=False):
     variables = load_variables(["PB", "Va", "mdot_a", "T_ec", "u_n", "l_t", "vAN1", "vAN2", "delta_z",
-                                "z0", "p0", "V_cc", "f_n", CONFIG_DIR / 'variables_v0.json')
+                                "z0", "p0", "V_cc", "f_n"], CONFIG_DIR / 'variables_v0.json')
     x = np.empty((2, len(variables)))
     for i, var in enumerate(variables):
         x[:, i] = var.sample_domain(2)
