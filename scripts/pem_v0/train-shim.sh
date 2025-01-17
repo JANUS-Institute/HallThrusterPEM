878ffba7097126c51135903c150ba66a5cefb48c
Joshua Eckels <eckelsjd@umich.edu>
2025-01-16 19:30:00 -0700

    build: update github action workflows

--- scripts/pem_v0/train-shim.sh
+++ scripts/pem_v0/train-shim.sh
@@ -5,7 +5,7 @@
 # Optional:
 # -d : discard outliers
 # -p : use variable PDF weighting
-./train.sh pem_v0/pem_v0_SPT-100.yml -c200 -t200 -e process -r1 -i150 -f both -N25 -m 1e-4 -C 5 -n 20\
+./train.sh pem_v0/pem_v0_SPT-100.yml -c200 -t200 -e process -r1 -i150 -f both -N25 -m 1e-4 -C 5 -n 20 \
                                      --targets T I_B0 I_d u_ion \
                                      --inputs P_b V_a a_1 a_2 \
                                      --outputs T I_B0 I_d u_ion \
