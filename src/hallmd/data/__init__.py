a526254b9a8fb206c14751aef80720eff11be0f3
Thomas Marks <marksta@umich.edu>
2025-01-17 17:59:14 -0500

    refactor: Overhaul data loaders and data format

--- src/hallmd/data/__init__.py
+++ src/hallmd/data/__init__.py
@@ -1,21 +1,175 @@
-"""The `hallmd.data` package contains a folder for each unique thruster. The experimental data for each thruster
-is further divided by folders for each individual paper or reference. The raw experimental data is contained within
-these folders in any arbitrary format (h5, json, csv, etc.). Each set of raw experimental data should come with a
-`dataloader.py` file that reads from the raw data into standardized Python objects. Any additional documentation
-for the datasets is encouraged (e.g. citations, descriptions, summaries, etc.) and can be included in the data folders.
+from dataclasses import dataclass, fields
+from pathlib import Path
+from typing import Any, Sequence
 
-## Thrusters
+import numpy as np
+import numpy.typing as npt
 
-### SPT-100
-Currently the only thruster with available data. Data for the SPT-100 comes from three sources:
+type Array = npt.NDArray[np.floating[Any]]
+type PathLike = str | Path
 
-1. [Diamant et al. 2014](https://arc.aiaa.org/doi/10.2514/6.2014-3710) - provides thrust, cathode coupling voltage, and ion current density data as a function of chamber background pressure.
-2. [Macdonald et al. 2019](https://arc.aiaa.org/doi/10.2514/1.B37133) - provides ion velocity profiles for varying chamber pressures.
-3. [Sankovic et al. 1993](https://www.semanticscholar.org/paper/Performance-evaluation-of-the-Russian-SPT-100-at-Sankovic-Hamley/81b7d985669b21aa1a8419277c52e7a879bf3b46) - provides thrust at varying operating conditions.
 
-Citations:
-``` title="SPT-100.bib"
---8<-- "hallmd/data/spt100/spt100.bib:citations"
-```
-"""
-# ruff: noqa: E501
+@dataclass(frozen=True)
+class OperatingCondition:
+    P_B: float
+    V_a: float
+    mdot_a: float
+
+
+@dataclass(frozen=True)
+class Measurement[T]:
+    mean: T
+    std: T
+
+    def __str__(self):
+        return f"(μ = {self.mean}, σ = {self.std})"
+
+
+@dataclass
+class ThrusterData:
+    # Cathode
+    V_cc: Measurement[float] | None = None
+    # Thruster
+    T: Measurement[float] | None = None
+    I_D: Measurement[float] | None = None
+    I_B0: Measurement[float] | None = None
+    eta_c: Measurement[float] | None = None
+    eta_m: Measurement[float] | None = None
+    eta_v: Measurement[float] | None = None
+    eta_a: Measurement[float] | None = None
+    uion_coords: Array | None = None
+    uion: Measurement[Array] | None = None
+    # Plume
+    div_angle: Measurement[float] | None = None
+    radius: float | None = None
+    jion_coords: Array | None = None
+    jion: Measurement[Array] | None = None
+
+    def __str__(self) -> str:
+        indent = "\t"
+        out: str = "ThrusterData(\n"
+        for field in fields(ThrusterData):
+            val = getattr(self, field.name)
+            if val is not None:
+                out += f"{indent}{field.name} = {val},\n"
+
+        out += ")\n"
+        return out
+
+
+def load(files: Sequence[PathLike] | PathLike) -> dict[OperatingCondition, ThrusterData]:
+    data: dict[OperatingCondition, ThrusterData] = {}
+    if isinstance(files, Sequence):
+        # Recursively load resources in this list (possibly list of lists)
+        for file in files:
+            data.update(load(file))
+    else:
+        data.update(_load_dataset(files))
+
+    return data
+
+
+def _load_dataset(file: PathLike) -> dict[OperatingCondition, ThrusterData]:
+    table = _table_from_file(file, delimiter=",", comments="#")
+    data: dict[OperatingCondition, ThrusterData] = {}
+
+    # Compute anode mass flow rate, if not present
+    mdot_a_key = "anode flow rate (mg/s)"
+    mdot_t_key = "total flow rate (mg/s)"
+    flow_ratio_key = "anode-cathode flow ratio"
+    keys = list(table.keys())
+
+    if mdot_a_key in keys:
+        mdot_a = table[mdot_a_key]
+    else:
+        if mdot_t_key in keys and flow_ratio_key in keys:
+            flow_ratio = table[flow_ratio_key]
+            anode_flow_fraction = flow_ratio / (flow_ratio + 1)
+            mdot_a = table[mdot_t_key] * anode_flow_fraction
+
+    # Get background pressure and discharge voltage
+    P_B = np.log10(table["background pressure (torr)"])
+    V_a = table["anode voltage (v)"]
+
+    num_rows = len(table[keys[0]])
+    row_num = 0
+    opcond_start_row = 0
+    opcond = OperatingCondition(P_B[0], V_a[0], mdot_a[0])
+
+    while True:
+        if row_num < num_rows:
+            next_opcond = OperatingCondition(P_B[row_num], V_a[row_num], mdot_a[row_num])
+            if next_opcond == opcond:
+                row_num += 1
+                continue
+
+        # either at end of operating condition or end of table
+        # fill up thruster data object for this row
+        data[opcond] = ThrusterData()
+
+        # Fill up data
+        # We assume all errors (expressed as value +/- error) correspond to two standard deviations
+        for key, val in table.items():
+            if key == "thrust (mn)":
+                # Load thrust data
+                T = val[opcond_start_row] * 1e-3  # convert to Newtons
+                T_std = table["thrust relative uncertainty"][opcond_start_row] * T / 2
+                data[opcond].T = Measurement(mean=T, std=T_std)
+
+            elif key == "anode current (a)":
+                # Load discharge current data
+                # assume a 0.1-A std deviation for discharge current
+                data[opcond].I_D = Measurement(mean=val[opcond_start_row], std=0.1)
+
+            elif key == "cathode coupling voltage (v)":
+                # Load cathode coupling data
+                V_cc = val[opcond_start_row]
+                V_cc_std = table["cathode coupling voltage absolute uncertainty (v)"][opcond_start_row] / 2
+                data[opcond].V_cc = Measurement(mean=V_cc, std=V_cc_std)
+
+            elif key == "ion velocity (m/s)":
+                # Load ion velocity data
+                uion: Array = val[opcond_start_row:row_num]
+                uion_std: Array = table["ion velocity absolute uncertainty (m/s)"][opcond_start_row:row_num] / 2
+                data[opcond].uion = Measurement(mean=uion, std=uion_std)
+                data[opcond].uion_coords = table["axial position from anode (m)"][opcond_start_row:row_num]
+
+            elif key == "ion current density (ma/cm^2)":
+                # Load ion current density data
+                jion: Array = val[opcond_start_row:row_num] * 10  # Convert to A / m^2
+                jion_std: Array = table["ion current density relative uncertainty"][opcond_start_row:row_num] * jion / 2
+                r = table["radial position from thruster exit (m)"][0]
+                jion_coords: Array = table["angular position from thruster centerline (deg)"][opcond_start_row:row_num]
+
+                # Keep only measurements at angles less than 90 degrees
+                keep_inds = jion_coords < 90
+                data[opcond].jion_coords = jion_coords[keep_inds] * np.pi / 180
+                data[opcond].radius = r
+                data[opcond].jion = Measurement(mean=jion[keep_inds], std=jion_std[keep_inds])
+
+        # Advance to next operating condition or break out of loop if we're at the end of the table
+        if row_num == num_rows:
+            break
+
+        opcond = next_opcond
+        opcond_start_row = row_num
+
+    return data
+
+
+def _table_from_file(file: PathLike, delimiter=",", comments="#") -> dict[str, Array]:
+    # Read header of file to get column names
+    # We skip comments (lines starting with the string in the `comments` arg)
+    header_start = 0
+    with open(file, "r") as f:
+        for i, line in enumerate(f):
+            if not line.startswith(comments):
+                header = line.rstrip()
+                header_start = i
+                break
+
+    column_names = header.split(delimiter)
+    data = np.genfromtxt(file, delimiter=delimiter, comments=comments, skip_header=header_start + 1)
+
+    table: dict[str, Array] = {column.casefold(): data[:, i] for (i, column) in enumerate(column_names)}
+    return table
--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,61 @@
 """Testing for loading experimental data."""
-from hallmd.data.loader import spt100_data
+
+import hallmd.data
+from hallmd.data import spt100
 
 
-def test_spt100():
-    data = spt100_data(['V_cc', 'T', 'jion', 'uion'])
-    v_cc = data['V_cc'][0]
-    assert v_cc['x'].shape[0] == v_cc['y'].shape[0] == v_cc['var_y'].shape[0]
+def test_spt100_macdonald2019():
+    expdata = hallmd.data.load(spt100.macdonald2019())
+    assert len(expdata.keys()) == 3
+    for opcond, data in expdata.items():
+        assert data.T is None
+        assert data.I_D is not None
+        assert data.uion is not None
+        assert data.uion_coords is not None
+        assert data.jion is None
+        assert data.jion_coords is None
+        assert data.V_cc is None
+        assert data.radius is None
+        assert opcond.mdot_a == 5.16
+        assert opcond.V_a == 300.0
+
+
+def test_spt100_diamant2014():
+    expdata_L3 = hallmd.data.load(spt100.diamant2014("L3"))
+    expdata_aerospace = hallmd.data.load(spt100.diamant2014("aerospace"))
+    expdata_all_explicit = hallmd.data.load(spt100.diamant2014(["L3", "aerospace"]))
+    expdata_all_implicit = hallmd.data.load(spt100.diamant2014())
+
+    assert len(expdata_all_explicit.keys()) == len(expdata_all_implicit.keys())
+    assert len(expdata_L3.keys()) + len(expdata_aerospace.keys()) == len(expdata_all_explicit.keys())
+
+    for opcond, data in expdata_all_implicit.items():
+        assert data.T is not None
+        assert data.V_cc is not None
+        assert data.I_D is not None
+        assert data.uion is None
+        assert data.uion_coords is None
+        if opcond in expdata_L3.keys():
+            assert data.jion is not None
+            assert data.jion_coords is not None
+            assert data.radius is not None
+        else:
+            assert data.jion is None
+            assert data.jion_coords is None
+            assert data.radius is None
+
+
+def test_spt100_sankovic1993():
+    expdata = hallmd.data.load(spt100.sankovic1993())
+
+    assert (len(expdata.keys())) > 100
+
+    for opcond, data in expdata.items():
+        assert data.T is not None
+        assert data.I_D is not None
+        assert data.V_cc is None
+        assert data.uion is None
+        assert data.uion_coords is None
+        assert data.jion_coords is None
+        assert data.jion is None
+        assert data.radius is None
