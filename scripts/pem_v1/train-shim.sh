d9169d6b6eb3d2ef1332457e3e11bf14f8ba7cbd
Thomas Marks <marksta@umich.edu>
2025-01-21 12:53:37 -0500

    feat: Add pemv1 config (#16)
    * Add pemv1 config
    
    * fix: thrust correction and v1 config
    
    ---------
    
    Co-authored-by: Joshua Eckels <eckelsjd@umich.edu>

---
+++ scripts/pem_v1/train-shim.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Shortcut script to call train.sh with most common arguments.
+# Run from the /scripts working directory.
+#
+# Optional:
+# -d : discard outliers
+# -p : use variable PDF weighting
+./train.sh pem_v1/pem_v1_SPT-100.yml -c200 -t200 -e process -r1 -i150 -f both -N25 -m 1e-4 -C 5 -n 20 \
+                                     --targets T I_B0 I_d u_ion \
+                                     --inputs P_b V_a a_1 a_2 \
+                                     --outputs T I_B0 I_d u_ion \
+                                     --show-model best worst \
+                                     --gen-cpus 36 --fit-cpus 16 --slice-cpus 36 \
+                                     --gen-time 00-01:00:00 --fit-time 00-02:15:00 --slice-time 00-00:15:00 "$@"
